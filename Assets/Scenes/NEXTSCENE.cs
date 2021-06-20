a4c6bd5db91a48f1af7d50aae35458b728166dbd
Saharatch Sangsuwan <saharatch@gmail.com>
2021-06-21 01:19:21 +0700

    add sector 4

---
+++ Assets/Scenes/NEXTSCENE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NEXTSCENE : MonoBehaviour
+{
+    public GameObject use;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
